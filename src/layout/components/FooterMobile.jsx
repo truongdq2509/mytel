4142c42b516321f0b0c080fcf6fd9c1366c66e21
truongdq <dinhquoctruong2509@gmail.com>
2024-07-07 11:48:52 +0700

    update

--- src/layout/components/FooterMobile.jsx
+++ src/layout/components/FooterMobile.jsx
@@ -4,32 +4,39 @@ import { urlPageBid, urlPageResult } from "../../helper/const";
 import _ from "lodash";
 import avatarDefault from "../../assets/images/avatarDefault.svg"
 import { checkImage } from '../../helper/helper';
+import { useTranslation } from 'react-i18next';
 
 function FooterMobile({ user }) {
 	const location = useLocation()
+	const { t } = useTranslation()
 	const { id = null, idResult = null } = useParams();
 	const listFooter = [
 		{
 			link: `${PATH.BID}/${urlPageBid.running}`,
 			classItem: "icon-bid",
-			linkActive: [`${PATH.BID}/${urlPageBid.running}`, `${PATH.BID}/${urlPageBid.upcoming}`,]
+			linkActive: [`${PATH.BID}/${urlPageBid.running}`, `${PATH.BID}/${urlPageBid.upcoming}`,],
+			title: t("header.bid")
 		},
 		{
 			link: `${PATH.RESULT}/${urlPageResult.all}`,
 			classItem: "icon-result",
-			linkActive: [`${PATH.RESULT}/${urlPageResult.all}`, `${PATH.RESULT}/${urlPageResult.the_winner}`, `${PATH.RESULT}/${urlPageResult.no_winner}`]
+			linkActive: [`${PATH.RESULT}/${urlPageResult.all}`, `${PATH.RESULT}/${urlPageResult.the_winner}`, `${PATH.RESULT}/${urlPageResult.no_winner}`],
+			title: t("header.result")
 		},
 		{
 			link: PATH.HOME,
 			classItem: "icon-home",
+			title: t("header.home")
 		},
 		{
 			link: PATH.RULE,
 			classItem: "icon-rule",
+			title: t("header.rule")
 		},
 		{
 			link: user ? PATH.ACCOUNT : PATH.LOGIN,
 			classItem: "icon-account",
+			title: t("header.account")
 		},
 	];
 
@@ -59,16 +66,21 @@ function FooterMobile({ user }) {
 					return (<Link
 						to={item.link}
 						key={`footer_item_${index}`}
-						style={{ backgroundImage: user && `url(${url})` }}
-						className={`footer-mobile-item ${item.classItem} ${getClassActive(item) ? 'active' : ''}`}
-					/>)
+						className={`footer-mobile-item  ${getClassActive(item) ? 'active' : ''}`}
+					>
+						<div className={`icon-footer ${item.classItem} ${getClassActive(item) ? 'active' : ''}`} style={{ backgroundImage: user && `url(${url})` }} />
+						<span className='text-footer'>{item.title}</span>
+					</Link>)
 				}
 				return (
 					<Link
 						to={item.link}
 						key={`footer_item_${index}`}
-						className={`footer-mobile-item ${item.classItem} ${getClassActive(item) ? 'active' : ''}`}
-					/>
+						className={`footer-mobile-item  ${getClassActive(item) ? 'active' : ''}`}
+					>
+						<div className={`icon-footer ${item.classItem} ${getClassActive(item) ? 'active' : ''}`} />
+						<span className='text-footer'>{item.title}</span>
+					</Link>
 				);
 			})}
 		</div>
